a12f5f56d1125387485711aebcbbd2cdb22751e7
Alyuun <alizeyun@gmail.com>
2023-02-07 13:19:23 +0000

    add file to config

--- config/database.js
+++ config/database.js
@@ -1,9 +1,21 @@
 import mysql from "mysql";
 
-export let pool  = mysql.createPool({
-  connectionLimit : 10000,
+export let pool = mysql.createPool({
+    connectionLimit : 10000,
     host: "db.3wa.io",// on rentre l'hôte, l'adresse url où se trouve la bdd
-    user: "", // identifiant BDD
-    password: "", // le password
-    database: "", // nom de la base de donnée
+    user: "alizebernardeau", // identifiant BDD
+    password: "bd47b11d8bbec14b24d22d9bf5819567", // le password
+    database: "alizebernardeau_tutotest", // nom de la base de donnée
 });
+
+// permet d'obtenir le resultat des requete sql async
+export const asyncQuery = async (sql, params = []) => {
+    return new Promise((resolve, reject)=>{
+        pool.query(sql,params, (error, result)=>{
+            if(error){
+                return reject(error);
+            }
+            return resolve(result);
+        });
+    });
+}
